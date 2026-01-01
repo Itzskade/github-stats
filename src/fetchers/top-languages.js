b389ff34ac79cf7eca80f724d1d759e32ba22326
Skade <itzskade@gmail.com>
2026-01-01 14:39:27 +0100

    Update top-languages.js

--- src/fetchers/top-languages.js
+++ src/fetchers/top-languages.js
@@ -1,5 +1,4 @@
 // @ts-check
-
 import { retryer } from "../common/retryer.js";
 import { logger } from "../common/log.js";
 import { excludeRepositories } from "../common/envs.js";
@@ -7,150 +6,83 @@ import { CustomError, MissingParamError } from "../common/error.js";
 import { wrapTextMultiline } from "../common/fmt.js";
 import { request } from "../common/http.js";
 
-/**
- * Top languages fetcher object.
- *
- * @param {any} variables Fetcher variables.
- * @param {string} token GitHub token.
- * @returns {Promise<import("axios").AxiosResponse>} Languages fetcher response.
- */
+/** @typedef {import("./types").TopLangData} TopLangData */
+
 const fetcher = (variables, token) => {
   return request(
     {
       query: `
-      query userInfo($login: String!) {
-        user(login: $login) {
-          repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
-            nodes {
-              name
-              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
-                edges {
-                  size
-                  node {
-                    color
-                    name
+        query userInfo($login: String!) {
+          user(login: $login) {
+            repositories(ownerAffiliations: OWNER, isFork: false, first: 50) {
+              nodes {
+                name
+                languages(first: 5, orderBy: {field: SIZE, direction: DESC}) {
+                  edges {
+                    size
+                    node {
+                      color
+                      name
+                    }
                   }
                 }
               }
             }
           }
         }
-      }
       `,
       variables,
     },
     {
       Authorization: `token ${token}`,
-    },
+    }
   );
 };
 
-/**
- * @typedef {import("./types").TopLangData} TopLangData Top languages data.
- */
+const fetchTopLanguages = async (username, exclude_repo = [], size_weight = 1, count_weight = 0) => {
+  if (!username) throw new MissingParamError(["username"]);
 
-/**
- * Fetch top languages for a given username.
- *
- * @param {string} username GitHub username.
- * @param {string[]} exclude_repo List of repositories to exclude.
- * @param {number} size_weight Weightage to be given to size.
- * @param {number} count_weight Weightage to be given to count.
- * @returns {Promise<TopLangData>} Top languages data.
- */
-const fetchTopLanguages = async (
-  username,
-  exclude_repo = [],
-  size_weight = 1,
-  count_weight = 0,
-) => {
-  if (!username) {
-    throw new MissingParamError(["username"]);
-  }
-
-  // ⚡ Obtener token desde variable de entorno
   const token = process.env.PAT_1;
-  if (!token) {
-    throw new CustomError(
-      "GitHub token (PAT_1) not found in environment variables.",
-      CustomError.GRAPHQL_ERROR
-    );
-  }
+  if (!token) throw new CustomError("GitHub token (PAT_1) not found.", CustomError.GRAPHQL_ERROR);
 
-  // Pasar token al retryer y fetcher
   const res = await retryer(fetcher, { login: username }, token);
 
   if (res.data.errors) {
     logger.error(res.data.errors);
-    if (res.data.errors[0].type === "NOT_FOUND") {
-      throw new CustomError(
-        res.data.errors[0].message || "Could not fetch user.",
-        CustomError.USER_NOT_FOUND,
-      );
-    }
-    if (res.data.errors[0].message) {
-      throw new CustomError(
-        wrapTextMultiline(res.data.errors[0].message, 90, 1)[0],
-        res.statusText,
-      );
-    }
     throw new CustomError(
-      "Something went wrong while trying to retrieve the language data using the GraphQL API.",
-      CustomError.GRAPHQL_ERROR,
+      res.data.errors[0]?.message || "GraphQL API error",
+      CustomError.GRAPHQL_ERROR
     );
   }
 
   let repoNodes = res.data.data.user.repositories.nodes;
-  /** @type {Record<string, boolean>} */
-  let repoToHide = {};
-  const allExcludedRepos = [...exclude_repo, ...excludeRepositories];
 
-  if (allExcludedRepos) {
-    allExcludedRepos.forEach((repoName) => {
-      repoToHide[repoName] = true;
+  // Filtrar repos excluidos
+  const allExcluded = [...excludeRepositories, ...exclude_repo];
+  repoNodes = repoNodes.filter(r => !allExcluded.includes(r.name));
+
+  // Reducir al top languages
+  const langMap = {};
+  repoNodes.forEach(repo => {
+    repo.languages.edges.forEach(edge => {
+      const name = edge.node.name;
+      if (!langMap[name]) langMap[name] = { name, color: edge.node.color, size: 0, count: 0 };
+      langMap[name].size += edge.size;
+      langMap[name].count += 1;
     });
-  }
-
-  // Filtrar repos a ocultar
-  repoNodes = repoNodes
-    .sort((a, b) => b.size - a.size)
-    .filter((name) => !repoToHide[name.name]);
-
-  let repoCount = 0;
-
-  repoNodes = repoNodes
-    .filter((node) => node.languages.edges.length > 0)
-    .reduce((acc, curr) => curr.languages.edges.concat(acc), [])
-    .reduce((acc, prev) => {
-      let langSize = prev.size;
-      if (acc[prev.node.name] && prev.node.name === acc[prev.node.name].name) {
-        langSize = prev.size + acc[prev.node.name].size;
-        repoCount += 1;
-      } else {
-        repoCount = 1;
-      }
-      return {
-        ...acc,
-        [prev.node.name]: {
-          name: prev.node.name,
-          color: prev.node.color,
-          size: langSize,
-          count: repoCount,
-        },
-      };
-    }, {});
-
-  Object.keys(repoNodes).forEach((name) => {
-    repoNodes[name].size =
-      Math.pow(repoNodes[name].size, size_weight) *
-      Math.pow(repoNodes[name].count, count_weight);
   });
 
-  const topLangs = Object.keys(repoNodes)
-    .sort((a, b) => repoNodes[b].size - repoNodes[a].size)
-    .reduce((result, key) => {
-      result[key] = repoNodes[key];
-      return result;
+  // Aplicar pesos
+  Object.values(langMap).forEach(lang => {
+    lang.size = Math.pow(lang.size, size_weight) * Math.pow(lang.count, count_weight);
+  });
+
+  // Ordenar
+  const topLangs = Object.values(langMap)
+    .sort((a, b) => b.size - a.size)
+    .reduce((acc, lang) => {
+      acc[lang.name] = lang;
+      return acc;
     }, {});
 
   return topLangs;
